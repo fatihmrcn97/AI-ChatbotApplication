4cf2351fdb0b9479865cd932de7bee6aa05c3ccf
fatihmrcn97 <fatihmercan7991@gmail.com>
2019-12-10 17:06:04 +0300

    First commit

---
+++ chatbot.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 23:49:55 2019
+
+@author: fatih
+"""
+
+# importing the libraries
+
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+###### PART 1 - DATA PREPROCESSING #####
+
+lines = open('movie_lines.txt',encoding ='utf-8' ,errors='ignore').read().split('\n')
+
+conservations = open('movie_conversations.txt',encoding ='utf-8' ,errors='ignore').read().split('\n')
+
+# Creating a dictionary that maps each line and its id
+
+id2line = {}
+for line in lines:
+    _line=line.split(' +++$+++ ')
+    if len(_line) ==5:
+        id2line[_line[0]] = _line[4]
+
+# Creating a list of all of the conservations
+        
+conversations_ids = []
+for conservations in conservations[:-1]:
+    _conversations = conservations.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversations_ids.append(_conversations.split(','))
+    
+# Getting seperately the questions and the answers
+questions= []
+ansewers=[]
+for conservations in conversations_ids:
+    for i in range(len(conservations)-1):
+        questions.append(id2line[conservations[i]])
+        ansewers.append(id2line[conservations[i+1]])
+# Cleaning of the text
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am" ,text)
+    text = re.sub(r"he's", "he is" ,text)
+    text = re.sub(r"it's", "it is" ,text)
+    text = re.sub(r"she's", "she is" ,text)
+    text = re.sub(r"that's", "that is" ,text)
+    text = re.sub(r"what's", "what is" ,text)
+    text = re.sub(r"where's", "where is" ,text)
+    text = re.sub(r"\'ll", " will" ,text)
+    text = re.sub(r"\'ve", " have" ,text)
+    text = re.sub(r"\'re", " are" ,text)
+    text = re.sub(r"\'d", " would" ,text)
+    text = re.sub(r"won't", "will not" ,text)
+    text = re.sub(r"can't", "can not" ,text)
+    text = re.sub(r"[-()\"#/@;:<>{}=~|.?,]", "" ,text)
+    return text
+# Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+# Cleaning the answers
+clean_answers =[]
+for answer in ansewers:
+    clean_answers.append(clean_text(answer))
+
+
+# Creating a dictionary that maps each word to its number of occurences
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] +=1
+
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] +=1            
+            
+
+# Creationg two dictionaris that map the questions word and the answers words to a unique integer
+threshold = 20
+questionswords2int ={}
+word_number = 0
+
+for word,count in word2count.items():
+    if count > threshold:
+        questionswords2int[word] = word_number
+        word_number +=1
+
+answerswords2int ={}
+word_number = 0
+
+for word,count in word2count.items():
+    if count > threshold:
+        answerswords2int[word] = word_number
+        word_number +=1
+
+
+# Adding the last tokens to these two dictionaries
+tokens =['<PAD>','<EOS>','<OUT>','<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int)+ 1
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int)+ 1
+
+# Creating the inverse dictionary of the answerwors2int dictionary
+        # w_i : values of the dic.    w = words of dict.
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+
+# Adding the End of String token to the end of every answer
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>' 
+
+# Translating all the questinons and the answers into integers
+    # and replacing all the words that were filtered out bt <OUT> 
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_into_int.append(ints)   
+
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+    answers_into_int.append(ints)
+
+# Sorting questions and answers by the length of questions
+
+sorted_clean_questions = []
+sorted_clean_answers= []
+
+for length in range(1,25 + 1):
+    for i in enumerate(questions_into_int):  # enumarete fonksiyonu index =0 and words [223,323,4545,65,65]
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
+        
+        
+##### Building the SEQ2SEQ Model
+            # Creating placeholders for the inputs and the targets
+        
+def model_inpputs():
+    inputs= tf.placeholder(tf.int32, [None,None], name='input')
+    targets= tf.placeholder(tf.int32, [None,None], name='target')
+    learningRate= tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob= tf.placeholder(tf.float32, name='keep_prob')
+    return inputs,targets,learningRate,keep_prob
+
+    
+# Preprocessin the targets
+    
+def preprocess_targets(targets, word2int ,batch_size):
+    left_side = tf.fill([batch_size,1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0] ,[batch_size,-1], [1,1])
+    preprocessed_targets = tf.concat([left_side,right_side],1)
+    return preprocessed_targets
+    
+# Creating the Encode RNN Layer
+    
+def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell,
+                                                       cell_bw= encoder_cell,
+                                                       sequence_length=sequence_length,
+                                                       inputs=rnn_inputs,
+                                                       dtype= tf.float32)
+    return encoder_state
+
+
+
+# Decoding the raining set
+
+def decode_training_set(encoder_state, decoder_cell, decoder_embeddeed_input ,sequence_length , decoding_scope, output_function, keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys,attention_values,attention_score_function,attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option='bahdanau',num_units=decoder_cell.output_size)
+    training_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],
+                                                                              attention_keys,
+                                                                              attention_values,
+                                                                              attention_score_function,
+                                                                              attention_construct_function,
+                                                                              name="attn_dec_train")
+    decoder_output, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                  training_decoder_function,
+                                                                  decoder_embeddeed_input,
+                                                                  sequence_length,
+                                                                  scope= decoding_scope)
+    decoder_output_dropout =tf.nn.dropout(decoder_output, keep_prob)
+    return output_function(decoder_output_dropout)
+
+
+# Decoding  the test / validation set
+# reduce overfitting and improve accuricy    
+
+
+
+def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix , sos_id, eos_id, maximum_length, num_words, sequence_length , decoding_scope, output_function, keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys,attention_values,attention_score_function,attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option='bahdanau',num_units=decoder_cell.output_size)
+    test_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_inference(output_function,
+                                                                              encoder_state[0],
+                                                                              attention_keys,
+                                                                              attention_values,
+                                                                              attention_score_function,
+                                                                              attention_construct_function,
+                                                                              decoder_embeddings_matrix , 
+                                                                              sos_id,
+                                                                              eos_id,
+                                                                              maximum_length, 
+                                                                              num_words,
+                                                                              name="attn_dec_inf")
+    test_predictions, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                    test_decoder_function,
+                                                                    scope= decoding_scope)
+    
+    return test_predictions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
